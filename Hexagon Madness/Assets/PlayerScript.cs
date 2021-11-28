dd5671192b356109c27aa437e97a5a723015ba83
Srmaquieira14 <Srmaquieira14@gmail.com>
2021-11-28 22:38:21 +0100

    Primer commit
    Primer commit. Pantalla de juego básica. Movimiento del personaje, colisiones, generación de enemigos y movimiento de la cámara

---
+++ Hexagon Madness/Assets/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float velocidad = 400f;
+    float movimiento = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimiento = Input.GetAxisRaw("Horizontal");
+        transform.RotateAround(Vector3.zero, Vector3.forward, movimiento * Time.fixedDeltaTime * -velocidad);
+    }
+
+    private void fixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movimiento*Time.fixedDeltaTime*-velocidad);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
