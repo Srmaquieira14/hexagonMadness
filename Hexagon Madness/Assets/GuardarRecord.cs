7483610c215125eec32a08cd8f9fc15fc50cb389
Srmaquieira14 <39341726+Srmaquieira14@users.noreply.github.com>
2022-12-11 18:42:57 +0100

    Gestión de escena guardar record y intentando POST
    Gestionada la escena de guardar record para que pueda mostrar mensajes de error, y que tenga todos los eventos listos. Intentando gestionar el POST desde unity a la API de REST

--- Hexagon Madness/Assets/GuardarRecord.cs
+++ Hexagon Madness/Assets/GuardarRecord.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Collections.Specialized;
 
 public class GuardarRecord : MonoBehaviour
 {
     public Text textoPuntuacion;
+    public Text textoError;
+    public string textoInputNombre;
+    private static readonly HttpClient client = new HttpClient();
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +32,34 @@ public class GuardarRecord : MonoBehaviour
     {
         FindObjectOfType<AudioManager>().Stop("MenuTheme");
         SceneManager.LoadScene("Game");
+    }
+
+    public async void botonGuardarRecord(){
+        if(textoInputNombre != ""){
+            textoError.text = "";
+            Debug.Log("Boton de guardar record ha sido pulsado " + textoInputNombre);
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:8080/records");
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //StreamReader reader = new StreamReader(response.GetResponseStream());
+            //string json = reader.ReadToEnd();
+            //Debug.Log(json);
+            var values = new Dictionary<string, string>
+            {
+                { "nombre", textoInputNombre },
+                { "valor", PantallaGameOver.puntuacionGlob.ToString() }
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync("http://localhost:8080/record", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+        } else {
+            textoError.text = "¡Introduzca un nombre!";
+            Debug.Log("La cadena de nombre no debe ir vacia");
+        }
+    }
+
+    public void leeInput(string s){
+        textoInputNombre = s;
+        Debug.Log(s);
     }
 
     public void ButtonClick()
